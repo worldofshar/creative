7dae3cf5b2dab38a1e817c03525152e0008068af
worldofshar <worldofshar@gmail.com>
2015-12-23 08:28:13 +0530

    Fix

--- creative/quote.py
+++ creative/quote.py
@@ -1,12 +1,12 @@
 from requests import get
 from lxml import html
-from datetime import date
+from datetime import date, timedelta
 
 
 def word_of_the_day():
     page = get("http://dictionary.reference.com/wordoftheday/")
     tree = html.fromstring(page.content)
-    day = str(date.today())
+    day = str(date.today() - timedelta(days=1))
     word = tree.xpath('//*[@id="wotd-' + day +
                       '"]/div[2]/div[1]/div/div[3]/div/strong')[0].text
     defn = tree.xpath('//*[@id="wotd-' + day +
